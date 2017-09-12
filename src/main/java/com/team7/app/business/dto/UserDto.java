7405047389419cb984b2cda0117fd0cd3f1f12d7
Alex Whitlatch <alexwhitlatch@hotmail.com>
2017-09-11 19:19:58 -0600

    added mustache dependency

--- src/main/java/com/team7/app/business/dto/UserDto.java
+++ src/main/java/com/team7/app/business/dto/UserDto.java
@@ -1,7 +1,15 @@
 package com.team7.app.business.dto;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class UserDto {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
 	private String name;
 	private int id;
 
