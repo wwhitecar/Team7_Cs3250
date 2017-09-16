0299f04b67e06a6fbfefae6c80e8afeda0a1487b
unknown <wwhiteca@msudenver.edu>
2017-09-16 16:51:47 -0600

    Updated index.html, create.html, added test classes, and updated Depen for junit

--- src/main/java/com/team7/app/controller/WebPageController.java
+++ src/main/java/com/team7/app/controller/WebPageController.java
@@ -1,12 +1,29 @@
 package com.team7.app.controller;
 
+import com.team7.app.business.dto.UserDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/")
 public class WebPageController {
 
 	@RequestMapping("/greeting")
 	String home() {return "Hello world!";}
 
+    @RequestMapping(value="/createUser", method=RequestMethod.GET)
+    public String createUser(@RequestParam("Id")int id, @RequestParam("First Name")String firstName,
+                             @RequestParam("Last Name")String lastName) {
+        UserDto user = new UserDto();
+        UserController uC = new UserController();
+        user.setFirstName(firstName);
+        user.setId(id);
+        user.setLastName(lastName);
+
+        return uC.createUser(user);
+    }
 }
---
+++ src/main/java/test/team7/app/controller/WebPageControllerTest.java
@@ -0,0 +1,16 @@
+package test.team7.app.controller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class WebPageControllerTest {
+    @Test
+    public void home() throws Exception {
+    }
+
+    @Test
+    public void createUser() throws Exception {
+    }
+
+}
