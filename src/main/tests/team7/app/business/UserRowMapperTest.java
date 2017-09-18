c0508b3197385608f3437639c1a1b5f6891dced3
Trevor Whitecar <wwhiteca@msudenver.edu>
2017-09-17 20:55:39 -0600

    Updateding testing

---
+++ src/main/tests/team7/app/business/UserRowMapperTest.java
@@ -0,0 +1,16 @@
+package team7.app.business;
+
+import com.team7.app.business.UserRowMapper;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UserRowMapperTest {
+
+    @Test
+    public void mapRowTest(){
+        UserRowMapper userRM = new UserRowMapper();
+
+    }
+
+}
