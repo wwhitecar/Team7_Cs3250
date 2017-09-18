3a3ecb2c5566260973a6886abd24e41b8d2af71a
Trevor Whitecar <wwhiteca@msudenver.edu>
2017-09-17 21:11:07 -0600

    More tests updated

--- src/main/tests/team7/app/controller/WebPageControllerTest.java
+++ src/main/tests/team7/app/controller/WebPageControllerTest.java
@@ -1,16 +1,15 @@
 package team7.app.controller;
 
+import com.team7.app.controller.WebPageController;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class WebPageControllerTest {
-    @Test
-    public void home() throws Exception {
-    }
 
     @Test
-    public void createUser() throws Exception {
+    public void webPageControllerTest(){
+        WebPageController wPC = new WebPageController();
+        assertTrue(wPC != null);
     }
-
 }
