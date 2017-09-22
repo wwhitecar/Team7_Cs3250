794d1713bc8b77f45d1d16d2ad1f73c15fa6187a
Trevor Whitecar <wwhiteca@msudenver.edu>
2017-09-22 16:24:26 -0600

    added Course class and controler, test case for controller added

---
+++ src/main/java/com/team7/app/business/dto/CourseDto.java
@@ -0,0 +1,132 @@
+package com.team7.app.business.dto;
+
+import java.util.ArrayList;
+
+/**
+ * CourseDto is a course that will be avalable for the students
+ * to be offered.
+ */
+public class CourseDto {
+
+    /**
+     * The department the course falls under.
+     */
+    private String department;
+
+    /**
+     * The course number to identify a course by more then
+     * just the name.
+     */
+    private int courseNumber;
+
+    /**
+     * The amount of credits the course is worth.
+     */
+    private int credits;
+
+    /**
+     * Description of the course.
+     */
+    private String description;
+
+    /**
+     * The learning goals of the course.
+     */
+    private String learningObjectives;
+
+    /**
+     * Prerequs for the course. Decided by class standing.
+     */
+    private String prereqs;
+    //TODO: Figure out what the prerequs are and how were
+    // are actually implemening them
+
+    /**
+     * The coreqs for the course.
+     */
+    private ArrayList<CourseDto> coreqs;
+
+    /**
+     * Empty Constructer.
+     */
+    public CourseDto() { }
+
+    /**
+     * Full param constructer.
+     * @param depart - department of class
+     * @param courseNum -coursenumber
+     * @param credit - credits the course is worth
+     * @param desctip - description of the course
+     * @param learningObj - learning objctive of course
+     * @param preReqs - prereqs for the course
+     * @param coReqs - coreqs for the course
+     */
+    public CourseDto(final String depart, final int courseNum,
+                     final int credit, final String desctip,
+                     final String learningObj, final String preReqs,
+                     final ArrayList<CourseDto> coReqs) {
+        this.department = depart;
+        this.courseNumber = courseNum;
+        this.credits = credit;
+        this.description = desctip;
+        this.learningObjectives = learningObj;
+        this.prereqs = preReqs;
+        this.coreqs = coReqs;
+    }
+
+    /**
+     * Getter for Department.
+     * @return department the course is in
+     */
+    public String getDepartment() {
+        return department;
+    }
+
+    /**
+     * Getter for coursenumber.
+     * @return number for the course
+     */
+    public int getCourseNumber() {
+        return courseNumber;
+    }
+
+    /**
+     * Getter for credits.
+     * @return the amount of credits the course is worth.
+     */
+    public int getCredits() {
+        return credits;
+    }
+
+    /**
+     * Getter for the descriptoin of the course.
+     * @return the description of the course.
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * Getter for the learningObjectives.
+     * @return learningObjectives for the course
+     */
+    public String getLearningObjectives() {
+        return learningObjectives;
+    }
+
+    /**
+     * Getter for the prereqs of the course.
+     * @return prereqs of the course
+     */
+    public String getPrereqs() {
+        return prereqs;
+    }
+
+    /**
+     * The coreqs requird for the course.
+     * @return corequs for thee course
+     */
+    public ArrayList<CourseDto> getCoreqs() {
+        return coreqs;
+    }
+}
---
+++ src/main/java/com/team7/app/controller/CourseController.java
@@ -0,0 +1,8 @@
+package com.team7.app.controller;
+
+/**
+ * Controlles communication between the webpages in the application
+ * and the database for the courses.
+ */
+public class CourseController {
+}
