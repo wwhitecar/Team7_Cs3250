04f500eb37029621c6f524597e5f43b908f3967e
Dan Zapfel <dzapfel@msudenver.edu>
2017-10-30 12:26:35 -0600

    weeks

--- src/main/java/com/team7/app/services/WeekServicesImpl.java
+++ src/main/java/com/team7/app/services/WeekServicesImpl.java
@@ -1,4 +1,33 @@
 package com.team7.app.services;
 
-public class WeekServicesImpl {
+import com.team7.app.business.dto.WeekDto;
+import com.team7.app.repositories.WeekRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * Implementation to communicate to the database.
+ * JDBC implementation.
+ */
+@Service
+public class WeekServicesImpl implements WeekServices {
+
+
+    /**
+     *
+     */
+    @Autowired
+    private WeekRepository weekRepository;
+
+    /**
+     *
+     *
+     */
+    public void setWeekService(final WeekRepository weekRepo) { this.weekRepository = weekRepo;}
+
+    /**
+     *
+     */
+    @Override
+    public Iterable<WeekDto> listAllWeeks() { return weekRepository.findAll(); }
 }
